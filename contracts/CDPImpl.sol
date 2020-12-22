693bdda1e0b792d311e496e8a3a0285cb5a7afec
Denis Tatarkin <denistatarkin11@gmail.com>
2020-12-22 21:15:35 +0300

    feat

--- contracts/CDPImpl.sol
+++ contracts/CDPImpl.sol
@@ -4,7 +4,7 @@ import './Ownable.sol';
 import './DAIToken.sol';
 
 contract CDPImpl is Initializable{
-    function initialize (address payable _borrow, uint32 _daiC, address _daiAddr) public {
+    function initialize (address payable _borrow, uint256 _daiC, address _daiAddr) public {
         _borrower = _borrow;
         _daiCount = _daiC;
         _isOpened = false;
@@ -13,28 +13,38 @@ contract CDPImpl is Initializable{
     }
 
     address payable private _borrower;
-    uint32 private _daiCount;
+    uint256 private _daiCount;
     bool private _isOpened;
     bool private _isClosed;
     uint256 private _deposit;
+    uint256 private _fiat;
     DAIToken _dai;
 
     function open() public payable{
         require(!_isOpened && !_isClosed);
         require(msg.sender == _borrower);
-        require(msg.value == getCourse(_daiCount));
+        _fiat = getCourse(_daiCount);
+        require(msg.value == _fiat);
         _isOpened = true;
         _deposit = msg.value;
         _dai.transfer(_borrower, _daiCount);
     }
 
-      function close() public{
+      function close(uint256 _daiC) public{
         require(_isOpened && !_isClosed);
         require(msg.sender == _borrower);
-        _isOpened = false;
-        _isClosed = true;
-        _dai.transferFrom(_borrower, address(this), _daiCount);
-        _borrower.transfer(getCourse(_daiCount));
+        require(_daiC <= _daiCount);
+        if(_daiC < _daiCount){
+            _dai.transferFrom(_borrower, address(this), _daiC);
+            _daiCount -= _daiC;
+        }
+        else
+        {
+            _isOpened = false;
+            _isClosed = true;
+            _borrower.transfer(getCourse(_daiC));
+            _daiCount = 0;
+        }
     }
 
     function getCourse(uint256 _daiC) private returns (uint256 course){
