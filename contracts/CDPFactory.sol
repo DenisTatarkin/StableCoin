d210c1aa2477882e040f12d80bb2697e16432ba1
Denis Tatarkin <denistatarkin11@gmail.com>
2020-12-29 02:45:22 +0300

    feat

--- contracts/CDPFactory.sol
+++ contracts/CDPFactory.sol
@@ -1,11 +1,22 @@
 pragma solidity ^0.5.1;
 import "./CDPImpl.sol";
+import './DAIToken.sol';
 
 contract CDPFactory {
+    constructor() public{
+        dai = new DAIToken();
+        daiAddress = address(dai);
+    }
+    
     address public implementation;
+    address public daiAddress;
+    DAIToken private dai;
+    address[] public cdps; //for testing!!!
 
     function createCDP(uint256 _daiCount) public returns (address){
-        address cdp = address(new CDPImpl(msg.sender, _daiCount));
+        address cdp = address(new CDPImpl(msg.sender, _daiCount, daiAddress));
+        dai.mint(cdp, _daiCount);
+        cdps.push(cdp);
         return cdp;
     }
 }
--- contracts/CDPImpl.sol
+++ contracts/CDPImpl.sol
@@ -4,11 +4,24 @@ import './DAIToken.sol';
 
 contract CDPImpl{
 
-     constructor(address _borrower, uint256 _daiCount) public {
+     constructor(address _borrower, uint256 _daiCount, address _daiAddress) public {
          borrower = _borrower;
          daiCount = _daiCount;
+         daiAddress = _daiAddress;
+         dai = DAIToken(_daiAddress);
+         isOpened = false;
     }
 
     address public borrower;
     uint256 public daiCount;
+    DAIToken private dai;
+    address public daiAddress;
+    bool public isOpened;
+    
+    function open() external{
+        require(msg.sender == borrower);
+        require(!isOpened);
+        dai.transfer(borrower, daiCount);
+        isOpened = true;
+    }
 }
